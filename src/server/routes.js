709ceea145f742b29f80a6b4d008b9cbbabaf389
Markus Voss <markus.voss@ehelse.no>
2017-03-26 20:53:32 +0200

    Commented in code

--- src/server/routes.js
+++ src/server/routes.js
@@ -1,9 +1,9 @@
 const router = require('koa-router')();
-//import FutureHomeController from '../futurehome/index';
-//import HarmonyHubController from '../harmonyHub/index';
+import FutureHomeController from '../futurehome/index';
+import HarmonyHubController from '../harmonyHub/index';
 
-//const f = new FutureHomeController();
-//const h = new HarmonyHubController();
+const f = new FutureHomeController();
+const h = new HarmonyHubController();
 
 /* eslint no-invalid-this: "off"  */
 router
@@ -11,16 +11,16 @@ router
     this.body = 'Hello world';
   })
   .get('/futurehome/turnOnHomeMode', function*() {
-//    f.turnOnHomeMode();
+    f.turnOnHomeMode();
     this.body = { message: 'success' };
   })
   .get('/futurehome/turnOnSleepMode', function*() {
-//    f.turnOnSleepMode();
+    f.turnOnSleepMode();
     this.body = { message: 'success' };
   })
   .get('/turnOffEverything', function*() {
-//    h.turnOffEverything();
-//    f.turnOnSleepMode();
+    h.turnOffEverything();
+    f.turnOnSleepMode();
     this.body = { message: 'success' };
   });
 export default router;
