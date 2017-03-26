4f10688eab2dcc396cf29363477c2b812e5f30d5
Markus Voss <markus.voss@ehelse.no>
2017-03-26 21:01:52 +0200

    Added cors

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,7 @@
 import Koa from 'koa';
 import Routes from './routes';
 import convert from 'koa-convert';
+import cors from 'koa-cors';
 
 const ports = {
   http: process.env.HTTP || 80, //eslint-disable-line no-process-env
@@ -8,6 +9,7 @@ const ports = {
 };
 
 const app = new Koa();
+app.use(convert(cors()));
 app.use(convert(Routes.routes()));
 app.use(convert(Routes.allowedMethods()));
 app.listen(ports.http);
